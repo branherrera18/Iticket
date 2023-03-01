c90cf7eac6934e7d3d4df73f54cf89566c551e48
Brandon Herrera Cerdas <brandonherrera011@hotmail.com>
2023-02-28 20:47:48 -0600

    eliminar y agregar funcionando

--- src/main/java/com/iticket/controller/ConciertoController.java
+++ src/main/java/com/iticket/controller/ConciertoController.java
@@ -8,34 +8,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ConciertoController {
-    
+
     @Autowired
     ConciertoService conciertoService;
-    
-    
-    @PostMapping("/guardarConcierto")
-    public String guardarConcierto(Concierto concierto){
-        conciertoService.guardarConcierto(concierto);
-        return "redirect:/";
+
+    @GetMapping("/conciertoN")
+    public String crearPersona(Model model) {
+        List<Concierto> listaConciertos = conciertoService.verConcierto();
+        model.addAttribute("concierto", new Concierto());//"persona"-> como lo leemos en el html //envia el objeto al html
+        model.addAttribute("conciertos", listaConciertos);
+        return "crear";
     }
 
     @GetMapping("/listaConciertos")
-    public String index(Model model){
+    public String index(Model model) {
         List<Concierto> listaConcierto = conciertoService.verConcierto();
-        model.addAttribute("titulo","Tabla Concierto");
-        model.addAttribute("concierto",listaConcierto);
+        model.addAttribute("titulo", "Tabla Concierto");
+        model.addAttribute("concierto", listaConcierto);
         return "listaConciertos";
     }
-    
+
     @GetMapping("/delete/{id}")
-    public String eliminar(Concierto concierto){
+    public String eliminar(Concierto concierto) {
         conciertoService.eliminarConcierto(concierto);
+        return "redirect:/listaConciertos";
+    }
+    
+    @PostMapping("/save")
+    public String guardarConcierto(@ModelAttribute Concierto concierto){
+        conciertoService.guardarConcierto(concierto);
         return "redirect:/";
     }
 }
